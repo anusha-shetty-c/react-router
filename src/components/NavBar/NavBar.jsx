8871ce1cd2404329c9143de1fdb91e7bb152f452
anusha-shetty-c <125472896+anusha-shetty-c@users.noreply.github.com>
2023-11-03 14:57:49 +0530

    routing

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function NavBar() {
+    return (
+        <div>
+            <Link to="/home"> Home</Link>
+            <Link to="/about"> About</Link>
+            <Link to="/contacts"> Contacts</Link>
+
+
+        </div>
+    )
+}
+
+export default NavBar
